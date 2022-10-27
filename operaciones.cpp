fcd835fb5faf7a23b2f7866847c2327b05784d0a
roque63 <roque@itesm.mx>
2022-10-27 15:19:48 -0500

    operaciones.cpp

---
+++ operaciones.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Prueba
+//
+//  Created by Ma. Guadalupe Roque Díaz de León on 26/10/22.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void operaNumeros( ){
+    //1.  Declararación de variables
+    
+
+    //2. leer 2 números x teclado
+   
+  
+    // 3. Leer opcion - no desplegar los mensajes
+    //cout << "<S> Mostrar la suma de 2 numeros" << endl <<
+    //"<R> Mostrar la resta de 2 numeros" << endl <<
+    //"<M> Mostrar la multiplicacion de 2 numeros" << endl;
+    
+  
+    
+    // Determinar la opcion elegida y llevar a acabo la operación aritmética, para
+    // desplegar el resultado
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    operaNumeros( )
+    
+    return 0;
+}
