1631acc23d799fd54e7ad12200ab26c0a1ab9935
roque63 <roque@itesm.mx>
2022-10-28 10:28:49 -0500

    Update serie.cpp

--- serie.cpp
+++ serie.cpp
@@ -1,10 +1,11 @@
 //
-//  main.cpp
-//  Prueba
+//  serie.cpp
+//  
 //
 //  Created by Ma. Guadalupe Roque Díaz de León on 26/10/22.
 //
 /* una serie aritmetica permite modelar problemasque pueden modelar fenómenos físicos y se define por:
+
 a + (a + d) + (a + 2d) + (a + 3d) + ... + [(a + (n - 1)d]
 
 Donde a es el primer término, d es la "diferencia común" y n es el número de términos que se van a sumar.
@@ -20,8 +21,8 @@ d
 n
 
 Dato de salida:
-
-
+la suma de la serie aritmética anterior.
+*/
 
 #include <iostream>
 #include <string>
