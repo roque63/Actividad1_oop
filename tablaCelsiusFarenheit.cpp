a29759e1e506b211c5d7d5c25ebdaa30c554255d
roque63 <roque@itesm.mx>
2022-10-28 10:33:45 -0500

    Update tablaCelsiusFarenheit.cpp

--- tablaCelsiusFarenheit.cpp
+++ tablaCelsiusFarenheit.cpp
@@ -5,20 +5,37 @@
 //  Created by Ma. Guadalupe Roque Díaz de León on 26/10/22.
 //
 /*
-El programa deberá solicitar al usuario:
+
+El programa deberá solicitar al usuario los siguientes
+- Datos de entrada:
 • El valor inicial en Celsius.
 • El número de conversiones que se harán.
 • El incremento entre los valores Celsius (valor entero).
 
-usa la fórmula farenheit = (9.0/5.0) celsius + 32
+Proceso:
+usa la fórmula para convertir de celsius a  farenheit = (9.0/5.0) celsius + 32
  
-- La salida debe ser : 
+- Datos de salida debe ser la siguiente tabla con todas las temperaturas en celsius y farenheit: 
  
-CELSIUS FARENHEIT
-valor1_c valor1_f
-.....
+                       CELSIUS FARENHEIT
+                       valor1_c valor1_f
+                       .....
 
-valorN_c valorN_f
+                       valorN_c valorN_f
+
+Caso de prueba1:
+Datos de entrada:
+40
+5
+20
+
+Datos de salida:
+CELSIUS FARENHEIT
+40 104
+45 113
+50 122
+55 131
+60 140
 */
 
 #include <iostream>
